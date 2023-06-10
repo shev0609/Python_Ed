a06b016251cae8cd08e6f211da233744d9eee4ad
shev0609 <m.lolita653@gmail.com>
2023-06-10 19:31:29 +0500

    Seminar_1

--- Seminar/S_1/s_3.py
+++ Seminar/S_1/s_3.py
@@ -6,7 +6,7 @@
 
 ### Решение
 year = int(input('Введите год: '))
-if year%4 == 0 and year%100 != 0 or year%400 == 0:
+if ((year%4 == 0) and (year%100 != 0)) or (year%400 == 0):
     print("год високосный")
 else:
     print("год не високосный")
