62ce4248089b7029cb0eeb8c4389ce90c8526597
shev0609 <m.lolita653@gmail.com>
2023-06-10 19:33:46 +0500

    Seminar_1

--- Seminar/S_1/s_3.py
+++ Seminar/S_1/s_3.py
@@ -10,3 +10,6 @@ if ((year%4 == 0) and (year%100 != 0)) or (year%400 == 0):
     print("год високосный")
 else:
     print("год не високосный")
+
+### Решение в одну строку
+### print("YES" if ((year%4 == 0) and (year%100 != 0)) or (year%400 == 0) else "NO")
